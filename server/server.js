eb2ee738b7a8f9094e5aeedc78bac3c78068addd
hinoob <reiopoldmaa113@gmail.com>
2026-04-23 20:36:46 +0300

    Add custom domain

--- server/server.js
+++ server/server.js
@@ -18,9 +18,9 @@ app.set("io", io);
 
 const corsOptions = {
     origin: [
-        "https://geoworld-psi.vercel.app"
+        "https://geoworld.byenoob.com"
     ],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
 };
